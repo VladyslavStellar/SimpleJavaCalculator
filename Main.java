0c99336ebefa775e10ae2ceb86da607999bdbfc0
Eugene <bielichenko777eugene@gmail.com>
2025-11-26 13:39:26 +0100

    Fix: corrected subtraction logic to addition in sum calculation

--- Main.java
+++ Main.java
@@ -2,9 +2,9 @@ public class Main {
     public static void main(String[] args) {
         int a = 10;
         int b = 5;
-        int sum = a - b; 
-        
-        System.out.println("Калькулятор версія 1.0");
-        System.out.println("Сума " + a + " + " + b + " дорівнює: " + sum);
+        int sum = a + b;// Fix: '+' instead of '-'
+
+        System.out.println("Calculator version 1.0");
+        System.out.println("Sum of " + a + " + " + b + " is: " + sum);
     }
 }
