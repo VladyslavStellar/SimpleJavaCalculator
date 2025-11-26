bbf1162f95f67cc96424f5c7f58534998f03b024
VladyslavStellar <eugene.stellar.tech@gmail.com>
2025-11-26 13:00:16 +0100

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 5;
+        int sum = a - b; 
+        
+        System.out.println("Калькулятор версія 1.0");
+        System.out.println("Сума " + a + " + " + b + " дорівнює: " + sum);
+    }
+}
